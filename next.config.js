09c6750b30933baaf97cd2f6ffdc43688950a397
Alinestradolini <122472427+Alinestradolini@users.noreply.github.com>
2025-07-10 15:43:23 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,9 @@
-module.exports = { reactStrictMode: true };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['vercel.app', 'nebula-x.vercel.app'],
+  },
+}
+
+module.exports = nextConfig;
